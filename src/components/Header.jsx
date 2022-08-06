0186e1127be44363076217679f5c7216014c60e1
Danny <dannythompson901>
2022-08-05 23:09:34 -0500

    MR-1 Adding the image to the header section and adjusting the content for responsiveness.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
-import style from "../assets/Header.module.css"
-
+import style from "../assets/css/Header.module.css"
+import HeaderImage from "../assets/images/header_learning.png"
 const Header = () => {
   return (
     <div>
       <div
-        className={`${style.headerContainer} text-3xl flex justify-center h-screen`}
+        className={`${style.headerContainer} mx-auto flex flex-row justify-center items-center h-screen w-screen`}
       >
-        <div className="font-sans text-6xl text-white drop-shadow-lg flex justify-end">
-          Community Trusted Resources. For you.
+        <div className="flex flex-row lg:ml-40">
+          <div className={`${style.imageContainer} w-full ml-80`}>
+            <img
+              src={HeaderImage}
+              className={`h-9/12 drop-shadow-2xl border border-gray-100`}
+            />
+          </div>
+          <div className="ml-24 font-sans sm:text-2xl lg:text-6xl text-white drop-shadow-lg flex justify-end">
+            Community Trusted Resources. For you.
+          </div>
         </div>
       </div>
     </div>
