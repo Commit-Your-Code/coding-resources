c589063b5858a272b5eb004fbcaab0031e367074
Danny <dannythompson901>
2022-08-05 21:56:55 -0500

    MR-1 Adjusted the background color to be more accessible and added CSS module for header section.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import style from "../assets/Header.module.css"
+
 const Header = () => {
   return (
     <div>
-      <div className="text-3xl bg-blue-100 flex justify-center h-screen">
+      <div
+        className={`${style.headerContainer} text-3xl flex justify-center h-screen`}
+      >
         <div className="font-sans text-6xl text-white drop-shadow-lg flex justify-end">
           Community Trusted Resources. For you.
         </div>
