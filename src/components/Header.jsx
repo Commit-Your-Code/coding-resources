9a413c3012f8ce31854c73f8249ea5d1a83e72be
Danny <dannythompson901>
2022-08-06 22:19:35 -0500

    MR-1 Adjusted header component and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import style from "../assets/css/Header.module.css"
 import HeaderImage from "../assets/images/header_learning.png"
+
 const Header = () => {
   return (
     <div>
       <div
         className={`${style.headerContainer} mx-auto flex flex-row justify-center items-center h-screen w-screen`}
       >
-        <div className="flex flex-row lg:ml-40">
-          <div className={`${style.imageContainer} w-full ml-80`}>
+        <div className="flex flex-row items-center md:ml-40">
+          <div className={`${style.imageContainer} ml-8 md:ml-80`}>
             <img
               src={HeaderImage}
-              className={`h-9/12 drop-shadow-2xl border border-gray-100`}
+              className={`h-full w-fit drop-shadow-2xl border border-gray-100`}
             />
           </div>
-          <div className="ml-24 font-sans sm:text-2xl lg:text-6xl text-white drop-shadow-lg flex justify-end">
+          <div className="ml-12 md:ml-24 font-sans text-2xl lg:text-6xl text-white drop-shadow-lg flex justify-end">
             Community Trusted Resources. For you.
           </div>
         </div>
