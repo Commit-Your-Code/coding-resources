334cdfd861ac23bb7a365c3a30dd7386495fde90
Danny <dannythompson901>
2022-08-10 08:42:30 -0500

    MR-1 Adding some styling and the action card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header/Header.jsx"
 import "./App.css"
 import FreeContent from "./components/FreeContent/FreeContent.jsx"
-
+import ActionCards from "./components/ActionCards/Card.jsx"
 function App() {
   return (
     <div className="App">
       <Header />
       <FreeContent />
+      <ActionCards />
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
       <div
         className={`${style.headerContainer} mx-auto flex flex-row justify-center items-center h-screen w-screen`}
       >
-        <div className="flex flex-row items-center md:ml-40">
-          <div className={`${style.imageContainer} ml-8 md:ml-80`}>
+        <div className="flex flex-row items-center lg:ml-40 ">
+          <div className={`${style.imageContainer} ml-8 md:ml-30 `}>
             <img
               src={HeaderImage}
-              className={`h-full w-fit drop-shadow-2xl border border-gray-100`}
+              className={`h-full w-60 drop-shadow-2xl border border-gray-100 rounded-lg `}
             />
           </div>
           <div className="ml-12 md:ml-24 font-sans text-2xl lg:text-6xl text-white drop-shadow-lg flex justify-end">
-            Community Trusted Resources. For you.
+            Community Trusted Resources. <br /> For you.
           </div>
         </div>
       </div>
