03d9f57a40a4709d1a25cd4ee0d7b9cb489c40b4
Danny <dannythompson901>
2022-08-10 08:47:04 -0500

    MR-1 Restructuring ActionCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header/Header.jsx"
 import "./App.css"
 import FreeContent from "./components/FreeContent/FreeContent.jsx"
-import ActionCards from "./components/ActionCards/Card.jsx"
+import ActionCard from "./components/ActionCard/ActionCard.jsx"
 function App() {
   return (
     <div className="App">
       <Header />
       <FreeContent />
-      <ActionCards />
+      <ActionCard />
     </div>
   )
 }
